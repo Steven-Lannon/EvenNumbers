ea513045b03ccff8fd53e0439168eacd23591fb5
Steven <lanns@farmingdale.edu>
2021-02-11 19:59:56 -0500

    added even numbers

--- EvenNumbers/src/evennumbers/EvenNumbers.java
+++ EvenNumbers/src/evennumbers/EvenNumbers.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package evennumbers;
 
 /**
@@ -14,9 +9,14 @@ public class EvenNumbers {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("test_git");
+        for(int i=0;  i<=100;  i++){
+         if (i % 2 != 0) {
+		System.out.println(" " + i);
+	   }
+      
+       }
     }
     
 }
